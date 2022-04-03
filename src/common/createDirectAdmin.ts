4457cf182131f852c6a43aef14d91ab2920b06b3
Yasniel <yasnielfajardoegues1408@gmail.com>
2022-04-03 00:24:12 -0400

    done

--- src/common/createDirectAdmin.ts
+++ src/common/createDirectAdmin.ts
@@ -1,16 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 import encryptPassword from './password/encryptPassword';
 
-
 const run = async () => {
   const orm = new PrismaClient();
   await orm.user.create({
-      data: {
-        password: await encryptPassword("admin123"),
-        name: "admin",
-        email: "admin@gmail.com",
-        role: 'ADMIN'
-      }
+    data: {
+      password: await encryptPassword('admin123'),
+      name: 'admin',
+      email: 'admin@gmail.com',
+      role: 'ADMIN'
+    }
   });
-}
-run()
+};
+run();
