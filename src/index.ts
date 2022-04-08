d59c7baa1a67759e1acb0f1e62ea9ce4884ee621
Yasniel <yasnielfajardoegues1408@gmail.com>
2022-04-08 18:26:28 +0200

    done

--- src/index.ts
+++ src/index.ts
@@ -35,6 +35,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+app.get('/hello', (req, res) => {
+  res.send('HOLA MUNDO');
+});
+
 (async () => {
   const apolloServer = new ApolloServer({
     typeDefs: [
